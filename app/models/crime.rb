d380fdad0fe92ddd37d32f5479699376cc883883
John Perry <jgperry@ajc.com>
2014-03-31 18:07:55 -0400

    aded current_month and by_month methods to Crime model

--- app/models/crime.rb
+++ app/models/crime.rb
@@ -1,2 +1,34 @@
+# == Schema Information
+#
+# Table name: crimes
+#
+#  id               :integer          not null, primary key
+#  offense_id       :integer
+#  rpt_date         :date
+#  occur_date       :date
+#  occur_time       :time
+#  poss_date        :date
+#  poss_time        :time
+#  beat             :integer
+#  location         :string(255)
+#  MaxOfnum_victims :integer
+#  Shift            :string(255)
+#  UC2_Literal      :string(255)
+#  neighborhood     :string(255)
+#  x                :integer
+#  y                :integer
+#  created_at       :datetime
+#  updated_at       :datetime
+#
+
 class Crime < ActiveRecord::Base
+
+  scope :by_month, -> (month, year) { where("month(occur_date) = #{month} and year(occur_date) = #{year}")}
+
+  def self.current_month
+    now = Time.new
+    month = now.month
+    year = now.year
+    self.by_month(month, year)
+  end
 end
