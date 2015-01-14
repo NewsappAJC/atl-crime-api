ed5ff74373d090b548075e311025d213a579ae6e
Ashlyn Still <ashlyn.still@gmail.com>
2015-01-14 12:24:21 -0500

    removed serializers

--- app/serializers/beat_serializer.rb
+++
@@ -1,7 +0,0 @@
-class BeatSerializer < ActiveModel::Serializer
-  attributes :zone, :beat, :population
-
-  has_many :crimes, :foreign_key => 'beat'
-  belongs_to :zone, :foreign_key => 'zone'
-  
-end
