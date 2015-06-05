67ee9d9143f1404009f630d5bbd8e189c27e3b6c
Ashlyn Still <ashlyn.still@gmail.com>
2015-06-05 18:37:18 -0400

    zone controller

--- app/controllers/zones_controller.rb
+++ app/controllers/zones_controller.rb
@@ -8,19 +8,34 @@ class ZonesController < ApplicationController
   #   render json: @zones, serializer: ZoneSerializer, callback: params[:callback]
   # end
 
+  def zone_test
+    @zones = Zone.all.map{ |z| { zone: z.zone, population: z.population.to_f, crime_percap: z.population.to_f===0 ? 0 : z.crimes.length/z.population.to_f } }
+    render json: @zones, callback: params[:callback]
+  end
+
   def all_zones
-  	@zones = Zone.all_zones
-  	render json: @zones, callback: params[:callback]
+    @zones = Zone.all_zones
+    render json: @zones, callback: params[:callback]
   end
 
   def show_all_crimes
-  	@zones = Zone.count_crimes
-  	render json: @zones, callback: params[:callback]
+    @zones = Zone.count_crimes
+    render json: @zones, callback: params[:callback]
+  end
+
+  def this_zone
+    @zones = Zone.count_crimes_byzone(params[:zone])
+    render json: @zones, callback: params[:callback]
+  end
+
+  def filter_zone
+    @zones = Zone.filter(params[:zone])
+    render json: @zones, callback: params[:callback]
   end
 
   def zone_crimes
-  	@zones = Zone.count_crimes_byzone(params[:zone])
-  	render json: @zones, callback: params[:callback]
+    @zones = Zone.all_crime(params[:zone])
+    render json: @zones, callback: params[:callback]
   end
 
   def zone_violent
